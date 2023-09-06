a43badacdfc3817b551021bc52d585df28a95ebe
Atharva01 <atharva.ad@gmail.com>
2023-09-06 21:53:06 +0200

    changes in model.py

--- source/dataset.py
+++ source/dataset.py
@@ -1,57 +1,32 @@
 import os
+import glob
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 class CustomSegmentationDataset(Dataset):
-    def __init__(self,data_dir,transform=None):
-        super(CustomSegmentationDataset,self).__init__()
+    def __init__(self, data_dir, transform=None):
+        super(CustomSegmentationDataset, self).__init__()
         self.data_dir = data_dir
         self.transform = transform
-        self.image_list = os.listdir(os.path.join(data_dir,"images"))
-        self.mask_list = os.listdir(os.path.join(data_dir,"mask"))
-        
+
+        # Use glob to get a list of image and mask file paths
+        self.image_list = glob.glob(os.path.join(data_dir, "images", "*.tif"))
+        self.mask_list = glob.glob(os.path.join(data_dir, "mask", "*.gif"))
+
     def __len__(self):
         return len(self.image_list)
-    
+
     def __getitem__(self, idx):
-        image_path = os.path.join(self.data_dir,"images",self.image_list[idx])
-        mask_path = os.path.join(self.data_dir,"mask",self.image_list[idx])
+        image_path = self.image_list[idx]
+        mask_path = self.mask_list[idx]
         image = Image.open(image_path)
         mask = Image.open(mask_path)
-        
+
+        image = transforms.ToTensor()(image)
+        mask = transforms.ToTensor()(mask)
+
         if self.transform:
             image = self.transform(image)
-            mask  = self.transform(mask)
-        return image,mask   
-'''
-#Image Transformations
-
-data_transforms = transforms.Compose([
-    transforms.Resize((512,512)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.ColorJitter(brightness=0.2,contrast=0.2,saturation=0.2,hue=0.1),
-    transforms.ToTensor(),
-    # The mean and standard deviation values used for normalization of pixel
-    # These values are commonly used for ImageNet Dataset
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-    
-# Paths for the train and test dataset directories
-train_data_dir = 'datasets_drive/training'
-test_data_dir = 'datasets_drive/test'
-
-# Instantiate  Dataset
-train_dataset = CustomSegmentationDataset(train_data_dir, transform=data_transforms)
-test_dataset = CustomSegmentationDataset(test_data_dir, transform=data_transforms)
-
-# Batch Size
-
-batch_size = 32
-
-# Data Loaders
-
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-'''
+            #mask = self.transform(mask)
+        return image, mask
--- source/model.py
+++ source/model.py
@@ -1,45 +1,92 @@
 import torch
 import torch.nn as nn
 
-# Define the U-Net architecture
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.batch_norm(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.batch_norm(x)
+        return x
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(EncoderBlock, self).__init__()
+        self.conv_block = ConvBlock(in_channels, out_channels)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        skip = x  # Save skip connection
+        x = self.pool(x)
+        return x, skip
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.upconv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv_block = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x, skip):
+        x = self.upconv(x)
+        x = torch.cat([x, skip], dim=1)  # Concatenate skip connection
+        x = self.conv_block(x)
+        return x
+
 class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UNet, self).__init__()
-        
-        # Encoder (Contracting Path)
-        self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.middle = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        
-        # Decoder (Expansive Path)
-        self.decoder = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(64, out_channels, kernel_size=2, stride=2)
-        )
-    
+
+        # Encoder
+        self.enc1 = EncoderBlock(in_channels, 64)
+        self.enc2 = EncoderBlock(64, 128)
+        self.enc3 = EncoderBlock(128, 256)
+        self.enc4 = EncoderBlock(256, 512)
+
+        # Bottleneck
+        self.bottleneck = ConvBlock(512, 1024)
+
+        # Decoder
+        self.dec4 = DecoderBlock(1024, 512)
+        self.dec3 = DecoderBlock(512, 256)
+        self.dec2 = DecoderBlock(256, 128)
+        self.dec1 = DecoderBlock(128, 64)
+
+        # Output
+        self.out_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
     def forward(self, x):
-        # Forward pass
-        encoder_output = self.encoder(x)
-        middle_output = self.middle(encoder_output)
-        decoder_output = self.decoder(middle_output)
-        return decoder_output
+        # Encoder
+        enc1, skip1 = self.enc1(x)
+        enc2, skip2 = self.enc2(enc1)
+        enc3, skip3 = self.enc3(enc2)
+        enc4, skip4 = self.enc4(enc3)
 
-# Create an instance of the U-Net model
-model = UNet(in_channels=3, out_channels=1)  # RGB images and 1 output channel for binary masks
+        # Bottleneck
+        bottleneck = self.bottleneck(enc4)
 
-# Print the model architecture
-print(model)
+        # Decoder
+        dec4 = self.dec4(bottleneck, skip4)
+        dec3 = self.dec3(dec4, skip3)
+        dec2 = self.dec2(dec3, skip2)
+        dec1 = self.dec1(dec2, skip1)
+
+        # Output
+        output = self.out_conv(dec1)
+
+        return output
+
+if __name__ == "__main__":
+    x = torch.randn((2, 3, 512, 512))
+    model = UNet(in_channels=3, out_channels=1)
+    y = model(x)
+    print(y.shape)
